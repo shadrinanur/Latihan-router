8ea846df070b66e3e551c1d39a3b323dcd733784
shadrinanur <nurazizahshadrina@gmail.com>
2023-08-04 08:38:46 +0700

    Latihan-router

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,6 @@ export default {
     font-family: Arial, Helvetica, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    color: green;
+    color:dodgerblue;
    }
 </style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,6 +1,7 @@
 <template>
   <center>
   <h1>Detail {{ detail.nama }}</h1>
+  <img :src="getImgSrc(detail.img)" alt="Category Image" style="width: 300px;" />
   <h3>Harga : {{ detail.harga }}</h3>
   <h4 style="font-style: italic;">Keterangan detail : {{ detail.keterangan }}</h4>
 </center>
@@ -14,11 +15,17 @@ export default {
   ],
   setup(props)
   {
+
+    const getImgSrc = (imgFileName) => {
+        return '../src/assets/img/' + imgFileName + '';
+       }
+
       const detail = produk["produk"].find(function (item) {
           return item.id == props.id_produk;
       });
       return {
-          detail
+          detail,
+          getImgSrc
       }
   }
 }
